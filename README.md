b992a99dad54decb02e1329961b055438faf3051
Dhivya <dhivyakotha@gmail.com>
2024-01-23 22:56:49 +0000

    updated readme file , added comments in html

--- README.md
+++ README.md
@@ -1,11 +1,54 @@
 # Daily Planner Application
-Daily planner application
 
-# About 
+## About 
 
-# Contents
+Daily planner is a simple calendar application to help record events for business hours in a given day to manage time effectively. The application is developed using HTML, JavaScript and JQuery and runs in the browser loaded dynamically with the recorded events for the given day.
 
-# Usage
+## Table of contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+    * [Screenshots](#screenshots)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+
+The application is compatible with most commonly used web browsers.
+
+The published website is accessible at   <a href=""><img src="https://img.shields.io/badge/see-published-site"  alt="Live Site" /></a>
 
 
-# License
+Github code for the application is available at
+    <a href="https://github.com/DhivyaKK/daily-planner-app.git"><img src="https://img.shields.io/badge/git-code-repository"  alt="github code repository" /></a>
+
+
+## Usage
+
+* Open the planner application in the browser. Current day is displayed at the top of the calender when a user opens the planner.
+* The timeblocks for standard business hours are presented when the user scrolls down.
+* The time blocks are color coded based on past, present, and future when the timeblock is viewed. The past time is color coded in grey,
+present time in red and furture time in green.
+* The user should be able to enter an event when clicking a timeblock.
+* Save the event in local storage when the save button is clicked in that timeblock.
+* Persist saved events between refreshes of a page
+
+### Screenshots
+
+The sample screen shots of the working of application is available below.
+
+Application landing page
+
+![Alt text](/assets/images/image.png)
+
+Enter a event in the textarea provided and click save button on the right end.
+
+![Alt text](/assets/images/image-1.png)
+
+Page refresh to load all the saved events in the planner.
+
+![Alt text](/assets/images/image-2.png)
+
+## License
+
+MIT License. Dhivya Kotha Kamalraj ©2024 All Rights Reserved.
--- assets/script/script.js
+++ assets/script/script.js
@@ -3,7 +3,7 @@ dayjs.extend(window.dayjs_plugin_advancedFormat);
 var currentDayEle = $("#currentDay");
 
 //var to store all the user tasks i.e text area vals
-var userSavedEvents = [];
+var savedEvents = [];
 var timeBlock;
 
 //var for html elements
@@ -22,6 +22,40 @@ $(window).on("load", function () {
   currentDayEle.text(formattedDate);
 
   //generate html for business hours 9am-6pm
+  generateHtmlForPlanner();
+
+  //function call to set the planner background color
+  setTextAreaBackground();
+
+  //On page refresh, presist the user values in textarea
+  savedEvents = JSON.parse(localStorage.getItem("userEvents")) || [];
+
+  for (let i = 0; i < savedEvents.length; i++) {
+    //console.log(savedEvents);
+    $("#" + savedEvents[i].eventKey)
+      .siblings(".description")
+      .val(savedEvents[i].eventText);
+  }
+
+  $(".saveBtn").on("click", function (e) {
+    var hourId = $(this).attr("id");
+    var textBlock = $(this).prev().val();
+    
+    //set an object to store the hour and user event 
+    let eventObj = {
+      eventKey: hourId,
+      eventText: textBlock,
+    };
+
+    savedEvents.push(eventObj);
+    //set the local storage var for later retrieval
+    localStorage.setItem("userEvents", JSON.stringify(savedEvents));
+  });
+});
+
+//generate html
+
+function generateHtmlForPlanner() {
   for (let i = 9; i <= 18; i++) {
     //create rows for planner
     row = $(`<div class="row">`);
@@ -29,46 +63,18 @@ $(window).on("load", function () {
       `<div class="col-2 time-block hour" id="${i}">${timeformat(i)} </div> `
     );
     eventCol = $(
-      `<textarea class="col-8 description"></textarea>`
+      `<textarea class="col-8 description" maxlength="500"></textarea>`
     );
     saveCol = $(
       ` <button id="${i}" class="col-2 saveBtn"><i class="fas fa-save"></i></button></div>`
     );
+    eventCol.css("color", "black");
     row.append(timeCol);
     row.append(eventCol);
     row.append(saveCol);
     $("#display-planner").append(row);
   }
-
-  timeBlock = $(".hour");
-  setTextAreaBackground(timeBlock);
-
-userSavedEvents = JSON.parse(localStorage.getItem("userEvents")) || [];
-
- for(let i = 0; i < userSavedEvents.length; i++)
- {
-    console.log(userSavedEvents);
-    $("#"+userSavedEvents[i].eventKey).siblings(".description").val(userSavedEvents[i].eventText);
- }
-
-$(".saveBtn").click(function(e){
-    var hourId = $(this).attr("id");
-    var textBlock = $(this).prev().val();
-    console.log(hourId, textBlock);
-
-     let eventObj ={
-        eventKey: hourId,
-        eventText: textBlock
-     }
-
-     userSavedEvents.push(eventObj);
-     localStorage.setItem("userEvents", JSON.stringify(userSavedEvents));
-     //console.log(userSavedEvents);
-})
-
-});
-
-
+}
 
 //show time as am /pm
 function timeformat(hour) {
@@ -84,9 +90,10 @@ function timeformat(hour) {
 }
 
 //check the hour block if its past(gray), present (red) or furture (green)
-//$.each(timeBlock, function()
-function setTextAreaBackground(timeBlock) {
 
+function setTextAreaBackground() {
+  timeBlock = $(".hour");
+  //$.each(timeBlock, function()
   timeBlock.each(function () {
     let getRowHour = parseInt($(this).attr("id"));
     if (currentHour === getRowHour) {
@@ -99,21 +106,3 @@ function setTextAreaBackground(timeBlock) {
   });
 }
 
-/* on click of textarea,class=event; on click of savebtn, set local storage with textarea contents.
-On page refresh, presist the user values in textarea */
-
-// $(".saveBtn").on("click", function (event) {
-//   //let textareaRef = $(this).prev();
-//   let textareaRef = $(this).attr("id");
-//   let userInputReminder = textareaRef.val();
-//   debugger;
-//   if (userInputReminder == "") {
-//     return;
-//   }
-
-//   let timeOfBlock = $(this).parent().children(0).attr("id");
-//   debugger;
-
-//   //store as a key value pair
-//   localStorage.setItem(timeOfBlock, userInputReminder);
-// });
